8143eec94b50db1f187a2a55a0fb5b1f4207670c
DrMorty <31830532+DrMorty@users.noreply.github.com>
2019-05-23 22:00:05 +0300

    Create Keyboard.h

---
+++ Keyboard.h
@@ -0,0 +1,17 @@
+#ifndef KEYBOARD_H
+#define KEYBOARD_H
+
+
+class Keyboard
+
+{
+    public:
+    void ifKeyPressed(key);
+    
+    private:
+      Keyboard();
+      
+}
+
+#endif#ifndef TIME_H
+#define TIME_H
