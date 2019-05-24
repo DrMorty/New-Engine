60a59cbe682b607bdcccbdb285a21d5b67ba0762
DrMorty <31830532+DrMorty@users.noreply.github.com>
2019-05-24 20:11:25 +0300

    Create KeyReading.cpp

---
+++ KeyReading.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+void Reading();
+{
+    if (sf::Keyboard::isKeyPressed(sf::Keyboard::Q))
+    {
+        std::cout<<"клавиша Q";
+    }
+    else if (sf::Keyboard::isKeyPressed(sf::Keyboard::W))
+    {
+        std::cout<<"клавиша W";
+    }
+    else if (sf::Keyboard::isKeyPressed(sf::Keyboard::E))
+    {
+        std::cout<<"клавиша E";
+    }
+    else if (sf::Keyboard::isKeyPressed(sf::Keyboard::E))
+    {
+        std::cout<<"клавиша E";
+    }
+}
