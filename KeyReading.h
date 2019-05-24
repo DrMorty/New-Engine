9d94e1c9ad17fe7d7b8af097db5a7ac28ec983a7
DrMorty <31830532+DrMorty@users.noreply.github.com>
2019-05-24 20:02:22 +0300

    Create KeyReading.h

---
+++ KeyReading.h
@@ -0,0 +1,16 @@
+#ifndef KEYREADING_H
+#define KEYREADING_H
+#include <SFML/Graphics.hpp>
+
+
+
+class KeyReading
+{
+  public:
+    void Reading();
+    
+  private:
+    KeyReading();
+    
+}
+#endif
